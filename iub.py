fc994f4e1cd7b78d90d2756e2865bd9837ef5fab
Nathan Yiangsupapaanontr <zartre.y@gmail.com>
2016-11-30 15:45:24 +0700

    Add main project file

---
+++ iub.py
@@ -0,0 +1,11 @@
+"""
+Internet Usage Behaviours of Thai Netizens
+
+A Project by:
+Nathan Yiangsupapaanontr,
+Rattanachat Sooksumpus,
+Thanathep Thaithae and
+Thanpisit Wattanasomvong
+
+For help, please refer to the documentation or type help.
+"""
